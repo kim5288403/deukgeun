ccdcab481477ea9e4554036833d42cc676edc1fc
limJH <kim5288403@naver.com>
2023-01-03 12:24:43 +0900

    project create

---
+++ src/main/java/com/example/deukgeun/DeukgeunApplication.java
@@ -0,0 +1,13 @@
+package com.example.deukgeun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeukgeunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeukgeunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/deukgeun/DeukgeunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deukgeun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeukgeunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
