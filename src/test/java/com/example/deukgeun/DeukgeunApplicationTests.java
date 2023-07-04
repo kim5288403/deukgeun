b50a5d73633ac05b06830ca0847f23aa3ffaf96e
limJH <kim5288403@naver.com>
2023-07-04 11:02:08 +0900

    Post controller TDD update

--- src/test/java/com/example/deukgeun/DeukgeunApplicationTests.java
+++ src/test/java/com/example/deukgeun/DeukgeunApplicationTests.java
@@ -1,7 +1,11 @@
 package com.example.deukgeun;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DeukgeunApplicationTests {
+    public static void main(String[] args) {
+        SpringApplication.run(DeukgeunApplicationTests.class, args);
+    }
 }
