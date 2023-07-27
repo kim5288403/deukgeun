ff449480277fc6969419bab768ed3f74d6ed05a2
limJH <kim5288403@naver.com>
2023-07-27 17:51:57 +0900

    Refactor directory structure to follow DDD principles

--- src/main/java/com/example/deukgeun/trainer/application/dto/request/validator/FileTypeValidator.java
+++ src/main/java/com/example/deukgeun/trainer/application/dto/request/validator/FileTypeValidator.java
@@ -1,6 +1,6 @@
 package com.example.deukgeun.trainer.application.dto.request.validator;
 
-import com.example.deukgeun.trainer.application.service.implement.ProfileApplicationServiceImpl;
+import com.example.deukgeun.global.util.MultipartFileUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,14 +10,12 @@ import javax.validation.ConstraintValidatorContext;
 @RequiredArgsConstructor
 public class FileTypeValidator implements ConstraintValidator<ValidFileType, MultipartFile> {
 
-    private final ProfileApplicationServiceImpl profileApplicationService;
-
     @Override
     public boolean isValid(MultipartFile value, ConstraintValidatorContext context) {
         if (value == null) {
             return false;
         }
 
-        return profileApplicationService.isSupportedContentType(value);
+        return MultipartFileUtil.isSupportedContentType(value);
     }
 }
