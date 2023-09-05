565f5380d115a941b3d70eb53376115691ac7449
limJH <kim5288403@naver.com>
2023-09-05 11:57:34 +0900

    Updated comments:applicant domain

--- src/main/java/com/example/deukgeun/applicant/infrastructure/api/IamPortApiService.java
+++ src/main/java/com/example/deukgeun/applicant/infrastructure/api/IamPortApiService.java
@@ -39,7 +39,13 @@ public class IamPortApiService {
         this.iamportClient = new IamportClient(API_KEY, API_SECRET);
     }
 
+    /**
+     * IamPort API 에서 인증 토큰을 얻기 위한 메서드입니다.
+     *
+     * @return IamPort API 에서 얻은 인증 토큰
+     */
     public String getIamPortAuthToken() {
+        // WebClient 생성합니다.
         WebClient webClient = WebClient.builder()
                 .baseUrl(GET_TOKEN_URL)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
@@ -59,7 +65,15 @@ public class IamPortApiService {
         return response.getResponse().getAccess_token();
     }
 
+    /**
+     * IamPort 에서 결제 취소를 요청하는 메서드입니다.
+     *
+     * @param request 결제 취소 요청에 필요한 정보를 담은 객체
+     * @return 결제 취소에 대한 응답 데이터
+     * @throws Exception 결제 취소 요청이 실패한 경우 발생하는 예외
+     */
     public IamPortCancelResponse cancelIamPort(CancelRequest request) throws Exception {
+        // WebClient를 생성합니다.
         WebClient webClient = WebClient.builder()
                 .baseUrl(CANCEL_URL)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
@@ -68,6 +82,7 @@ public class IamPortApiService {
                 .mutate()
                 .build();
 
+        // 결제 취소 요청을 준비합니다.
         IamPortCancelResponse response = webClient
                 .post()
                 .uri(UriBuilder::build)
@@ -77,6 +92,7 @@ public class IamPortApiService {
                 .block();
 
         assert response != null;
+        // 결제 취소가 실패한 경우 예외를 발생시킵니다.
         if (response.getCode() == 1) {
             throw new Exception(response.getMessage());
         }
@@ -84,6 +100,14 @@ public class IamPortApiService {
         return response;
     }
 
+    /**
+     * imp_uid 를 사용하여 Iamport 에서 결제 정보를 조회하는 메서드입니다.
+     *
+     * @param imp_uid Iamport 에서 사용되는 고유한 결제 식별자
+     * @return 결제 정보를 포함한 Iamport 응답 데이터
+     * @throws IamportResponseException Iamport API 응답 처리 중 발생한 예외
+     * @throws IOException 입출력 예외가 발생한 경우
+     */
     public IamportResponse<Payment> paymentByImpUid(String imp_uid) throws IamportResponseException, IOException {
         return iamportClient.paymentByImpUid(imp_uid);
     }
--- src/main/java/com/example/deukgeun/applicant/infrastructure/persistence/adapter/ApplicantRepositoryAdapter.java
+++ src/main/java/com/example/deukgeun/applicant/infrastructure/persistence/adapter/ApplicantRepositoryAdapter.java
@@ -22,28 +22,60 @@ import java.util.Optional;
 public class ApplicantRepositoryAdapter implements ApplicantRepository {
     private final ApplicantJpaRepository applicantJpaRepository;
 
+    /**
+     * 공고 식별자(jobId)로 매칭 정보(matchInfoId)가 있는지 확인합니다.
+     *
+     * @param jobId 확인하려는 공고의 고유 식별자
+     * @return 공고에 매칭 정보가 있으면 true, 그렇지 않으면 false를 반환합니다.
+     */
     @Override
     public boolean existsByJobIdAndMatchInfoIdNotNull(Long jobId) {
         return applicantJpaRepository.existsByJobIdAndMatchInfoIdNotNull(jobId);
     }
 
+    /**
+     * 공고(jobId)에 대한 지원자가 지정된 트레이너(trainerId)인지 여부를 확인합니다.
+     *
+     * @param jobId     공고 고유 식별자
+     * @param trainerId 트레이너 고유 식별자
+     * @return 해당 공고에 대한 트레이너의 지원 여부를 나타내는 boolean 값
+     */
     @Override
     public boolean existsByJobIdAndTrainerId(Long jobId, Long trainerId) {
         return applicantJpaRepository.existsByJobIdAndTrainerId(jobId, trainerId);
     }
 
+    /**
+     * 지원자 식별자(id)를 사용하여 지원자 정보를 조회합니다.
+     *
+     * @param id 조회하려는 지원자의 고유 식별자
+     * @return 지원자 정보를 포함한 Optional 객체
+     */
     @Override
     public Optional<Applicant> findById(Long id) {
         Optional<ApplicantEntity> applicantEntity = applicantJpaRepository.findById(id);
         return applicantEntity.map(this::convert);
     }
 
+    /**
+     * 공고 식별자(jobId)로 페이징된 지원자 목록을 조회합니다.
+     *
+     * @param jobId       공고 고유 식별자
+     * @param pageRequest 페이지 요청 정보 (페이지 번호, 페이지 크기 등)
+     * @return 페이지에 포함된 지원자 목록
+     */
     @Override
     public Page<Applicant> findPageByJobId(Long jobId, PageRequest pageRequest) {
         Page<ApplicantEntity> applicantEntities = applicantJpaRepository.findPageByJobId(jobId, pageRequest);
         return applicantEntities.map(this::convert);
     }
 
+    /**
+     * 지원자 정보를 저장합니다.
+     *
+     * @param applicant 저장할 지원자 정보
+     * @return 저장된 지원자 정보
+     */
     @Override
     public Applicant save(Applicant applicant) {
         ApplicantEntity applicantEntity = applicantJpaRepository.save(convert(applicant));
